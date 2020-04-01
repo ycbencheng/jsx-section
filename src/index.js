5a63a7069e8011784f5ee6e516e63f52333a422e
ycbencheng <bcyc2004@gmail.com>
2020-04-01 07:16:09 -0600

    Create a form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hello there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+      <button>Submit</button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
